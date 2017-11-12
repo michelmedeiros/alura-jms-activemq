262833cf1fa8198cb18fbf6b38f41759dd772367
Michel Medeiros <michel.tds@gmail.com>
2017-11-12 10:15:22 -0200

    Criando produtor e consumidor

--- src/main/java/br/com/caelum/jms/Consumer.java
+++ src/main/java/br/com/caelum/jms/Consumer.java
@@ -1,28 +1,55 @@
 package br.com.caelum.jms;
 
+import org.springframework.stereotype.Component;
+
 import javax.jms.*;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import java.util.Enumeration;
+import java.util.Scanner;
 
 /**
  * Created by Michel Medeiros on 07/11/2017.
  */
+@Component
 public class Consumer {
-    public static void main(String[] args) throws NamingException, JMSException {
-        /*
-         */
 
+    @SuppressWarnings("resource")
+    public static void main(String[] args) throws Exception {
 
-        InitialContext initialContext = new InitialContext();
-        ConnectionFactory factory = (ConnectionFactory) initialContext.lookup("ConnectionFactory");
+        InitialContext context = new InitialContext();
+        ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+
         Connection connection = factory.createConnection();
         connection.start();
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-        Destination fila = (Destination) initialContext.lookup("financeiro");
-        MessageConsumer consumer = session.createConsumer(fila);
-        Message receive = consumer.receive();
-        System.out.println("Recebendo mensagem: " + receive);
+        Destination fila = (Destination) context.lookup("financeiro");
+        MessageConsumer consumer = session.createConsumer(fila );
+
+        //Visualizar mensagem sem consumir
+        QueueBrowser browser = session.createBrowser((Queue) fila);
+        Enumeration msgs = browser.getEnumeration();
+        while (msgs.hasMoreElements()) {
+            TextMessage msg = (TextMessage) msgs.nextElement();
+            System.out.println("Message: " + msg.getText());
+        }
+
+        //Consumir de mensagens
+        consumer.setMessageListener(new MessageListener() {
+            @Override
+            public void onMessage(Message message) {
+                TextMessage textMessage = (TextMessage) message;
+                try {
+                    System.out.println("Recebendo msg: " + textMessage.getText());
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        new Scanner(System.in).nextLine();
+        session.close();
         connection.close();
-        initialContext.close();
+        context.close();
     }
 }
---
+++ src/main/java/br/com/caelum/jms/Producer.java
@@ -0,0 +1,39 @@
+package br.com.caelum.jms;
+
+import java.util.Scanner;
+
+import javax.jms.*;
+import javax.naming.InitialContext;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Michel Medeiros on 07/11/2017.
+ */
+@Component
+public class Producer {
+
+    public static void main(String[] args) throws Exception {
+
+        InitialContext context = new InitialContext();
+        ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+
+        Connection connection = factory.createConnection();
+        connection.start();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        Destination fila = (Destination) context.lookup("financeiro");
+
+        MessageProducer producer = session.createProducer(fila);
+
+        for(int i =1; i <= 1000; i++) {
+            Message message = session.createTextMessage("<pedido><id>" + i +"</id></pedido>");
+            producer.send(fila, message);
+            System.out.println("Send to queue"  + message);
+        }
+
+        session.close();
+        connection.close();
+        context.close();
+    }
+}
